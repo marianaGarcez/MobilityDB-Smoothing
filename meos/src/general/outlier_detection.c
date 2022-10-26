214a36a0566d502b1c85df8893dc3cfcf43db40f
Mariana Duarte <marianaduarte@macbook-pro.wit.ulb.ac.be>
2022-10-26 15:43:34 +0200

    replace tsequence
    Signed-off-by: Mariana Duarte <marianaduarte@macbook-pro.wit.ulb.ac.be>

--- meos/src/general/outlier_detection.c
+++ meos/src/general/outlier_detection.c
@@ -78,6 +78,11 @@ double diff_seconds(TInstant t0, TInstant t1){
     //TODO set seconds
     return (t_1 - t_0)*60;
 }
+
+double frand() {
+    return 2*((rand()/(double)RAND_MAX) - 0.5);
+}
+
 // For each individual trajectory, 
 //filter out the trajectory points that are considered noise 
 //or outliers
@@ -123,7 +128,7 @@ TSequence filter_trajectory_heuristic(TSequence t, int max_speed, int include_lo
     int32 lx;
     double dt;
 
-    //can I know the size of a trajectory?
+    //Size of a trajectory - how many instants in a sequence
     lx = t.count;
 
     //TInstant tint= t.period;
@@ -156,6 +161,60 @@ TSequenceSet filter_trajectory_heuristic_set(TSequenceSet t, int max_speed, int 
 
 TSequence Kalman_filter_CV(TSequence t,float process_noise_std, int measurement_noise_std){
 
+    //initial values for the kalman filter
+    float x_est_last = 0;
+    float P_last = 0;
+    //the noise in the system
+    float Q = 0.022;
+    float R = 0.617;
+    
+    float K;
+    float P;
+    float P_temp;
+    float x_temp_est;
+    float x_est;
+    float z_measured; //the 'noisy' value we measured
+    float z_real = 0.5; //the ideal value we wish to measure
+    
+    srand(0);
+    
+    //initialize with a measurement
+    x_est_last = z_real + frand()*0.09;
+    
+    float sum_error_kalman = 0;
+    float sum_error_measure = 0;
+    
+    for (int i=0;i<30;i++) {
+        //do a prediction
+        x_temp_est = x_est_last;
+        P_temp = P_last + Q;
+        //calculate the Kalman gain
+        K = P_temp * (1.0/(P_temp + R));
+        //measure
+        z_measured = z_real + frand()*0.09; //the real measurement plus noise
+        //correct
+        x_est = x_temp_est + K * (z_measured - x_temp_est); 
+        P = (1- K) * P_temp;
+        //we have our new system
+        
+        printf("Ideal    position: %6.3f \n",z_real);
+        printf("Mesaured position: %6.3f [diff:%.3f]\n",z_measured,fabs(z_real-z_measured));
+        printf("Kalman   position: %6.3f [diff:%.3f]\n",x_est,fabs(z_real - x_est));
+        
+        sum_error_kalman += fabs(z_real - x_est);
+        sum_error_measure += fabs(z_real-z_measured);
+        
+        //update our last's
+        P_last = P;
+        x_est_last = x_est;
+    }
+    
+    printf("Total error if using raw measured:  %f\n",sum_error_measure);
+    printf("Total error if using kalman filter: %f\n",sum_error_kalman);
+    printf("Reduction in error: %d%% \n",100-(int)((sum_error_kalman/sum_error_measure)*100));
+    
+    
+    return 0;
 }
 
 
